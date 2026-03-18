4fdc6fcaf417cd9914e2bccc6bb554d3c5263c8f
109-creator <luougi09@gmail.com>
2026-03-18 18:29:09 +0900

    2026/03/18 18:29:02

---
+++ js/script4.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function printNumber(n) {
+    for (let num = 0; num <= n; num++) {
+        console.log(num)
+        
+    }
+}
+
+printNumber(10);
+printNumber(1);
+printNumber(20);
