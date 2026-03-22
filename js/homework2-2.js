5b2ffb34946f0047c4a21abb9528e0b08d4a395b
109-creator <luougi09@gmail.com>
2026-03-22 09:23:25 +0900

    2026/03/22 09:23:21

---
+++ js/homework2-2.js
@@ -0,0 +1,17 @@
+function isPrime(n) {
+    if (n < 2) { return false; }
+    if (n === 2) { return true; }
+    for (let k = 0; k <= n; k++) {
+        if (n % k === 0) { return false; }
+        break
+    }
+    return true;
+}
+
+function listPrimes(limit) {
+    for (let i = 0; i < limit; i++) {
+        if (isPrime(i) === true) { console.log(i); }    
+    }
+}
+
+listPrimes
